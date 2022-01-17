841106c3453a67cb34c605488b2a1d10a87737c6
huigang39 <huigang39@outlook.com>
2022-01-17 20:01:22 +0800

    test

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/huigang/codespace/ros/arduino_mpu6050_temperature/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
