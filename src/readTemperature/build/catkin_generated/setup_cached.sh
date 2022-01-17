68c9dee3744facf5519a1e50aaf40174c3806be5
huigang39 <huigang39@outlook.com>
2022-01-17 20:42:11 +0800

    Arduino 连接 MPU6050 读取温度传给 ROS

--- src/readTemperature/build/catkin_generated/setup_cached.sh
+++ src/readTemperature/build/catkin_generated/setup_cached.sh
@@ -9,8 +9,7 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/huigang/codespace/ros/arduino_mpu6050_temperature/src/readTemperature/build/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/huigang/codespace/ros/arduino_mpu6050_temperature/src/readTemperature/build/devel/lib:$LD_LIBRARY_PATH"
-export PATH='/opt/ros/noetic/bin:/home/huigang/.nvm/versions/node/v17.3.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:~/devtool/flutter/bin:~/.platformio/penv/bin:~/devtool/flutter/bin:~/.platformio/penv/bin'
 export PKG_CONFIG_PATH="/home/huigang/codespace/ros/arduino_mpu6050_temperature/src/readTemperature/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
-export PWD='/home/huigang/codespace/myblog/source/_posts'
+export PWD='/home/huigang/codespace/ros/arduino_mpu6050_temperature'
 export ROSLISP_PACKAGE_DIRECTORIES='/home/huigang/codespace/ros/arduino_mpu6050_temperature/src/readTemperature/build/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/huigang/codespace/ros/arduino_mpu6050_temperature/src/readTemperature:$ROS_PACKAGE_PATH"
--- src/readTemperature/src/main.cpp
+++ src/readTemperature/src/main.cpp
@@ -1,1 +1,47 @@
+// #define USE_USBCON
 #include <Arduino.h>
+#include <ros.h>
+#include <Wire.h>
+#include <MPU6050.h>
+#include <std_msgs/Float32.h>
+
+// 实例化 MPU6050 对象
+MPU6050 mpu;
+
+// 实例化节点句柄
+ros::NodeHandle nh;
+
+// 声明温度数据类型
+std_msgs::Float32 temperature_msg;
+
+// 实例化发布者
+ros::Publisher temperature_pub("temperature", &temperature_msg);
+
+void setup()
+{
+    // 初始化 Wire 库
+    Wire.begin();
+
+    // 初始化 MPU6050
+    mpu.begin();
+
+    // 初始化 ROS
+    nh.initNode();
+    nh.advertise(temperature_pub);
+}
+
+void loop()
+{
+    // 读取温度
+    float temperature = mpu.readTemperature();
+
+    // 将温度数据赋值给消息
+    temperature_msg.data = temperature;
+
+    // 发布温度数据
+    temperature_pub.publish(&temperature_msg);
+
+    // 每隔 100ms 发布一次
+    nh.spinOnce();
+    delay(100);
+}
